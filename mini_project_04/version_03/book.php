281dd60ce6ed815ad794989f4634b70a47b9118c
awatkin <144458571+awatkin@users.noreply.github.com>
2025-10-13 15:52:35 +0100

    started mini project 4 version 3, moved machines as xampp broke on teacher machine.

---
+++ mini_project_04/version_03/book.php
@@ -0,0 +1,48 @@
+<?php // This open the php code section
+
+session_start();
+require_once "assets/common.php";
+
+echo "<!DOCTYPE html>";  # essential html line to dictate the page type
+
+echo "<html>";  # opens the html content of the page
+
+    echo "<head>";  # opens the head section
+
+        echo "<title> Version 3</title>";  # sets the title of the page (web browser tab)
+        echo "<link rel='stylesheet' type='text/css' href='css/styles.css' />";  # links to the external style sheet
+
+    echo "</head>";  # closes the head section of the page
+
+echo "<body>";  # opens the body for the main content of the page.
+
+echo "<div class='container'>";
+
+    require_once "assets/topbar.php";
+
+    require_once "assets/nav.php";
+
+echo "<div class='content'>";
+    echo "<br>";
+
+    echo "<h2> Primary Oaks - Appointment Booking System</h2>";  # sets a h2 heading as a welcome
+echo "<br>";
+
+echo "<form action='' method='post'>";
+
+
+
+echo "</form>";
+
+echo "<br>";
+
+echo usermessage();
+
+echo "</div>";
+
+echo "</div>";
+
+echo "</body>";
+
+echo "</html>";
+?>
---
+++ mini_project_04/version_03/bookings.php
@@ -0,0 +1,45 @@
+<?php // This open the php code section
+
+session_start();
+require_once "assets/common.php";
+
+echo "<!DOCTYPE html>";  # essential html line to dictate the page type
+
+echo "<html>";  # opens the html content of the page
+
+    echo "<head>";  # opens the head section
+
+        echo "<title> Version 3</title>";  # sets the title of the page (web browser tab)
+        echo "<link rel='stylesheet' type='text/css' href='css/styles.css' />";  # links to the external style sheet
+
+    echo "</head>";  # closes the head section of the page
+
+echo "<body>";  # opens the body for the main content of the page.
+
+echo "<div class='container'>";
+
+    require_once "assets/topbar.php";
+
+    require_once "assets/nav.php";
+
+echo "<div class='content'>";
+    echo "<br>";
+
+    echo "<h2> Primary Oaks - Your Bookings</h2>";  # sets a h2 heading as a welcome
+
+    echo "<p class='content'> Boiler plate text about the doctors surgery </p>";
+
+echo "<p class='content'> You have to be registered to login and book </p>";
+
+echo "<br>";
+
+echo usermessage();
+
+echo "</div>";
+
+echo "</div>";
+
+echo "</body>";
+
+echo "</html>";
+?>
